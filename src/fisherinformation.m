6209a18f08065c4e965a4f65b6d47c9b1546e13d
kai <supon3060@gmail.com>
2022-05-13 11:26:53 +0800

    Reformat

--- src/fisherinformation.m
+++ src/fisherinformation.m
@@ -11,15 +11,15 @@ dpds = @(s) imag(p(s + 1i * STEP_SIZE)) / STEP_SIZE;  % dp(s)/ds
 % FIXME: The integral result is not stable
 
 % 'ArrayValued' == false may be faster than 'ArrayValued' == true when number of data point > 30000
-% integrand_func = @(s)  (dpds(s).^2) ./ p(s);  % integrand function: [dp(s)/ds]^2 / p(s)
+% integrand_func = @(s) (dpds(s).^2) ./ p(s);  % integrand function: [dp(s)/ds]^2 / p(s)
 % fim = integral(integrand_func, min(data) - pd.BandWidth, max(data) + pd.BandWidth);
 
 % `quadgk` is faster than `integral(..., 'ArrayValued', false)`
-integrand_func = @(s)  (dpds(s).^2) ./ p(s);  % integrand function: [dp(s)/ds]^2 / p(s)
+integrand_func = @(s) (dpds(s).^2) ./ p(s);  % integrand function: [dp(s)/ds]^2 / p(s)
 fim = quadgk(integrand_func, min(data) - pd.BandWidth, max(data) + pd.BandWidth, 'MaxIntervalCount', 1000);
 
 % Causes long execution time when number of data point > 30000 and 'ArrayValued' == true
-% integrand_func = @(s)  (dpds(s)^2) / p(s);  % integrand function: [dp(s)/ds]^2 / p(s)
+% integrand_func = @(s) (dpds(s)^2) / p(s);  % integrand function: [dp(s)/ds]^2 / p(s)
 % fim = integral(integrand_func, min(data) - pd.BandWidth, max(data) + pd.BandWidth, 'ArrayValued', true);
 
 
