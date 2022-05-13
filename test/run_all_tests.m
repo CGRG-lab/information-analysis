a5bdc6769d62aabc5d28d5d56d4a5fa4e61e6155
kai <supon3060@gmail.com>
2022-05-13 11:29:01 +0800

    Add test for `shannonentropy`

--- test/run_all_tests.m
+++ test/run_all_tests.m
@@ -1,3 +1,6 @@
 clear; clc; close;
+
 suite = {'test_informationanalysis'};
 result = runtests(suite);
+rt = table(result);
+display(rt);
--- test/test_informationanalysis.m
+++ test/test_informationanalysis.m
@@ -3,17 +3,30 @@ tests = functiontests(localfunctions);
 end
 
 function test_fisherinformation(testCase)
-num_points = 10000;
+fprintf('    Running test_fisherinformation\n');
 
+num_points = 10000;
 standard_deviation = 1;
 theoretical_fim = 1 / standard_deviation.^2;
 relative_tolerance = 0.03;
 data = randn(num_points, 1);
-
-tic;
 fim = fisherinformation(data);
-toc;
 
-fprintf('relative error with theoretical fim: %f\n', abs(fim - theoretical_fim) / abs(theoretical_fim))
+fprintf('    relative error with theoretical fim: %f\n', abs(fim - theoretical_fim) / abs(theoretical_fim))
 verifyLessThanOrEqual(testCase, abs(fim - theoretical_fim) / abs(theoretical_fim), relative_tolerance)
 end
+
+function test_shannonentropy(testCase)
+fprintf('    Running test_shannonentropy\n');
+
+num_points = 10000;
+standard_deviation = 1;
+theoretical_se = 1 / 2 * log(2 * pi * exp(1) * standard_deviation^2);
+relative_tolerance = 0.03;
+data = randn(num_points, 1);
+se = shannonentropy(data);
+
+fprintf('    relative error with theoretical se: %f\n', abs(se - theoretical_se) / abs(theoretical_se))
+verifyLessThanOrEqual(testCase, abs(se - theoretical_se) / abs(theoretical_se), relative_tolerance)
+end
+
