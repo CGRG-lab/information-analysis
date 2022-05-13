1d6ca97af391e0d2840cbc670756b191a9b29411
kai <supon3060@gmail.com>
2022-05-13 11:35:50 +0800

    Update comment

--- src/shannonentropy.m
+++ src/shannonentropy.m
@@ -5,6 +5,6 @@ function se = shannonentropy(data)
 pd = fitdist(data, 'Kernel');  % fit the kernel dendity function
 p = @(s) min(max(pdf(pd, s), eps(0)), Inf);  % eps(0) <= p(s) <= Inf
 
-integrand_func = @(s) p(s) .* log(p(s));  % integrand function: [dp(s)/ds]^2 / p(s)
+integrand_func = @(s) p(s) .* log(p(s));  % integrand function: p(s) * log(p(s))
 se = -1 * integral(integrand_func, min(data) - pd.BandWidth, max(data) + pd.BandWidth);
 end
