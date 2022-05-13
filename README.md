b77dda548fef255b01e0452fc4b8fc88507fa330
kai <supon3060@gmail.com>
2022-05-13 14:54:55 +0800

    Use finite difference
    Because the complex step differentiation is for analytic functions,
    we fall back to using finite differences.

--- README.md
+++ README.md
@@ -11,9 +11,7 @@ Implementation of Shannon entropy and Fisher information with Matlab.
 1. Modify m-files in `test` directory to fit your test cases.
 1. Run `run_all_tests` located in the `test` directory to run the tests
 
-## Note
-Since some functions are not supported by automatic differentiation, and finite difference increase round-off error (but decrease truncation error) when using small step size, we use complex step differentiation.
-
 ## References:
 - [MathWorks Blogs: Complex Step Differentiation](https://blogs.mathworks.com/cleve/2013/10/14/complex-step-differentiation/)
+- http://www.johnlapeyre.com/posts/complex-step-differentiation/
 - Telesca, L. & Lovallo, M. (2017) On the performance of Fisher Information Measure and Shannon entropy estimators. Physica A: Statistical Mechanics and its Applications, 484, 569–576, Elsevier.
--- src/fisherinformation.m
+++ src/fisherinformation.m
@@ -5,22 +5,21 @@ function fim = fisherinformation(data)
 pd = fitdist(data, 'Kernel');  % fit the kernel dendity function
 p = @(s) min(max(pdf(pd, s), eps(0)), Inf);  % eps(0) <= p(s) <= Inf
 
-STEP_SIZE = 1e-15;
-dpds = @(s) imag(p(s + 1i * STEP_SIZE)) / STEP_SIZE;  % dp(s)/ds
-
-% FIXME: The integral result is not stable
+STEP_SIZE = sqrt(8*eps());  % http://www.johnlapeyre.com/posts/complex-step-differentiation/
+dpds = @(s) (p(s + STEP_SIZE) - p(s - STEP_SIZE)) / (2 * STEP_SIZE);  % dp(s)/ds
 
 % 'ArrayValued' == false may be faster than 'ArrayValued' == true when number of data point > 30000
 % integrand_func = @(s) (dpds(s).^2) ./ p(s);  % integrand function: [dp(s)/ds]^2 / p(s)
 % fim = integral(integrand_func, min(data) - pd.BandWidth, max(data) + pd.BandWidth);
 
 % `quadgk` is faster than `integral(..., 'ArrayValued', false)`
+
 integrand_func = @(s) (dpds(s).^2) ./ p(s);  % integrand function: [dp(s)/ds]^2 / p(s)
+warning('off','MATLAB:quadgk:MaxIntervalCountReached');
 fim = quadgk(integrand_func, min(data) - pd.BandWidth, max(data) + pd.BandWidth, 'MaxIntervalCount', 1000);
+warning('on','MATLAB:quadgk:MaxIntervalCountReached');
 
 % Causes long execution time when number of data point > 30000 and 'ArrayValued' == true
 % integrand_func = @(s) (dpds(s)^2) / p(s);  % integrand function: [dp(s)/ds]^2 / p(s)
 % fim = integral(integrand_func, min(data) - pd.BandWidth, max(data) + pd.BandWidth, 'ArrayValued', true);
-
-
 end
