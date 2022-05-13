bea06bcdfecec90489d02ec5f748a78dde583a38
okatsn <okatsn@gmail.com>
2022-05-13 16:16:48 +0800

    add some descriptions and comments

--- src/fisherinformation.m
+++ src/fisherinformation.m
@@ -1,3 +1,6 @@
+% data has to be column vector (N by 1)
+% Modify 'MaxIntervalCount' or 'AbsoluteTolerance' (maybe
+% 'RelativeTolerance' in quadgk) if it is too slow.
 function fim = fisherinformation(data)
 %Calculate "Fisher Information" (¡ì [dp(s)/ds]^2 * ds/p(s))
 %   fisherinformation(data)
@@ -17,7 +20,9 @@ dpds = @(s) (p(s + STEP_SIZE) - p(s - STEP_SIZE)) / (2 * STEP_SIZE);  % dp(s)/ds
 integrand_func = @(s) (dpds(s).^2) ./ p(s);  % integrand function: [dp(s)/ds]^2 / p(s)
 warning('off','MATLAB:quadgk:MaxIntervalCountReached');
 fim = quadgk(integrand_func, min(data) - pd.BandWidth, max(data) + pd.BandWidth, 'MaxIntervalCount', 1000);
-warning('on','MATLAB:quadgk:MaxIntervalCountReached');
+warning('on','MATLAB:quadgk:MaxIntervalCountReached'); % if 'MaxIntervalCount' 
+% is reached but accepted tolerance has not been reached, then warning.
+% Warning disabled here.
 
 % Causes long execution time when number of data point > 30000 and 'ArrayValued' == true
 % integrand_func = @(s) (dpds(s)^2) / p(s);  % integrand function: [dp(s)/ds]^2 / p(s)
--- test/test_informationanalysis.m
+++ test/test_informationanalysis.m
@@ -1,7 +1,11 @@
+% `run_all_tests` in command window to run all tests.
 function tests = test_informationanalysis
 tests = functiontests(localfunctions);
 end
 
+% In the paper of Telesca 2017, Figure 10 and 11,
+% with N = 10^4 (num_points=10000), the relative error of FIM vs \sigma
+% (Fig. 10; red line) is less than 0.03 
 function test_fisherinformation(testCase)
 fprintf('    Running test_fisherinformation\n');
 
@@ -16,13 +20,15 @@ fprintf('    relative error with theoretical fim: %f\n', abs(fim - theoretical_f
 verifyLessThanOrEqual(testCase, abs(fim - theoretical_fim) / abs(theoretical_fim), relative_tolerance)
 end
 
+% See the paper of Telesca 2017, Figure 11 (redline N=10000),
 function test_shannonentropy(testCase)
 fprintf('    Running test_shannonentropy\n');
 
+
 num_points = 10000;
 standard_deviation = 1;
-theoretical_se = 1 / 2 * log(2 * pi * exp(1) * standard_deviation^2);
-relative_tolerance = 0.03;
+theoretical_se = 1 / 2 * log(2 * pi * exp(1) * standard_deviation^2); % the correct answer
+relative_tolerance = 0.01;
 data = randn(num_points, 1);
 se = shannonentropy(data);
 
